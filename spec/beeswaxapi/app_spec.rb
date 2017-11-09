b4f483bb7bf302f593026042a6c61b22e26a1100
Valentyn Ostakh <valikos.ost@gmail.com>
2017-11-09 21:15:23 +0200

    Add tests for configuration

---
+++ spec/beeswaxapi/app_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe BeeswaxAPI::App do
+  %i[
+    basic_auth cookie_auth cookie_file base_uri 
+    user_name password verbose_logger
+  ].each do |config|
+    it "contains #{config} config option" do
+      expect(described_class.settings).to include config
+    end
+  end
+
+  it 'setups basic_auth configuration' do
+    described_class.config.basic_auth = true
+  end
+
+  it 'setups cookie_auth configuration' do
+    described_class.config.cookie_auth = true
+  end
+
+  it 'setups cookie_file configuration' do
+    described_class.config.cookie_file = '/tmp/cookies.txt'
+  end
+
+  it 'setups base_uri configuration' do
+    described_class.config.base_uri = 'https://sandbox.beeswax.api.com/rest'
+  end
+
+  it 'setups user_name configuration' do
+    described_class.config.user_name = 'user@name.com'
+  end
+
+  it 'setups password configuration' do
+    described_class.config.password = 'password'
+  end
+
+  it 'setups verbose_logger configuration' do
+    described_class.config.verbose_logger = true
+  end
+end
